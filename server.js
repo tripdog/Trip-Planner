6d9cdc7ed4337df4d639e172bf0ad2843682a3fe
Tom Phillips <eflips@yahoo.com>
2021-06-03 11:23:21 -0700

    Expenses endpoint is hooked up

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require("express");
-const mongo = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017";
+const express = require("express")
+const mongo = require("mongodb").MongoClient
+const url = "mongodb://localhost:27017"
 
-const app = express();
+const app = express()
+app.use(express.json())
 
-app.use(express.json());
-
-let db, trips, expenses;
-
-app.listen(3005, () => console.log("Server ready"));
+//set up the DB
+let db, trips, expenses
 
 mongo.connect(
   url,
@@ -18,41 +16,59 @@ mongo.connect(
   },
   (err, client) => {
     if (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
-    db = client.db("tripcost");
-    trips = db.collection("trips");
-    expenses = db.collection("expenses");
+    db = client.db("tripcost")
+    trips = db.collection("trips")
+    expenses = db.collection("expenses")
   }
 );
 
-
-
-//endpoints
+//create the endpoints
 app.post("/trip", (req, res) => {
   const name = req.body.name;
-    trips.insertOne({ name: name }, (err, result) => {
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ ok: true });
+  });
+});
+
+app.get("/trips", (req, res) => {
+    trips.find().toArray((err, items) => {
         if (err) {
             console.error(err)
             res.status(500).json({ err: err })
             return
         }
-        console.log(result)
-        res.status(200).json({ ok: true })
-  });
-});
-
-app.get("/trips", (req, res) => {
-    /* */
+        res.status(200).json({trips: items})
+    })
 });
 
 app.post("/expense", (req, res) => {
-  /* */
+    expenses.insertOne({
+        trip: req.body.trip,
+        date: req.body.date,
+        amount: req.body.amount,
+        category: req.body.category,
+        description: req.body.description,
+    },
+        (err, result) => {
+            if (err) {
+                console.err(err)
+                res.status(500).json({ err: err })
+            }
+            resizeTo.status(200).json({ ok: true })
+        }
+    )
 });
 
 app.get("/expenses", (req, res) => {
   /* */
 });
 
-
+app.listen(3000, () => console.log("Server is ready Captain!"))
